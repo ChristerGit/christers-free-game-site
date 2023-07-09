f7efca1cc2b72e3f479e518200c3c086c60a0525
ChristerGit <karl.andersson997@gmail.com>
2023-07-09 12:13:01 +0200

    first update of site v2

---
+++ src/pages/steam_games.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+import Layout from "../components/layout"
+
+// Step 2: Define your component
+const IndexPage = () => {
+  return (
+    <main>
+        <Layout>
+        </Layout>
+    </main>
+  )
+}
+
+// You'll learn about this in the next task, just copy it for now
+export const Head = () => <title>Home Page</title>
+
+// Step 3: Export your component
+export default IndexPage
