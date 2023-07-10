9c3535ef36bae71420a5c67786ca7cd0eee5cfee
ChristerGit <karl.andersson997@gmail.com>
2023-07-10 22:27:39 +0200

    static game test again

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,5 @@ const express = require(`express`)
 
 // Enable development support for serving HTML from `./static` folder
 exports.onCreateDevServer = ({ app }) => {
-  app.use(express.static(`static`))
+  app.use(express.static("static"))
 }
